95cb1f7286d8c0e7d955f2cf281e27a2d06b7925
aethlis <84690253+aethlis@users.noreply.github.com>
2021-06-17 12:10:52 +0900

    almost finished

--- SSAP 세척/ssap-wash.js
+++
@@ -1,9 +0,0 @@
-
-
-
-const timeButton = document.querySelectorAll('timeBox')
-const timeText = document.querySelectorAll('timeText')
-
-timeButton.addEventListener('click', function show() {
-    timeButton.style.display = "flex"
-})
