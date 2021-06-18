b721a746921f8b42dd9b4ab8b05a109e703a649e
aethlis <84690253+aethlis@users.noreply.github.com>
2021-06-18 12:24:52 +0900

    2021-06-18
    made some changes to the media query, also some fixes within the css code

--- SSAP 세척/ssapwash.js
+++ SSAP 세척/ssapwash.js
@@ -1,3 +1,11 @@
-var timeLineText = document.getElementsByClassName('timeText')
-var toggleButton = document.getElementsByClassName('timeBox')
-
+function buttonadd() {
+    var i = 0
+    var timeBox = document.querySelectorAll(".timeBox")[i]
+    for (i=0; i<timeLineText.length; i++) {
+        timeBox[i].addEventListener('click', show())
+        if (event.target === timeBox[0]) {
+            var t1 = document.getElementsByClassName("t1")
+            t1.setAttribute('style', 'display:none;')
+        }
+    }
+}
