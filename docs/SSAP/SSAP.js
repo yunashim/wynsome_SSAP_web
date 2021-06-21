0624cc93364c048f42ee0650c0b351359b558325
jo-yuni <84690460+jo-yuni@users.noreply.github.com>
2021-06-22 01:12:50 +0900

    헤더 최종최종최종

--- docs/SSAP/SSAP.js
+++ docs/SSAP/SSAP.js
@@ -1,3 +1,6 @@
+document.getElementById('toggle-menu').addEventListener('click', function () {
+    document.querySelector('.newmenu').classList.toggle('hidden');
+})
 function move() {
     document.querySelector(".firstcircle").classList.toggle("go")
     document.querySelector(".firsttext").classList.toggle("see")
@@ -6,7 +9,3 @@ function move1() {
     document.querySelector(".secondcircle").classList.toggle("go")
     document.querySelector(".secondtext").classList.toggle("see")
 }
-
-// document.getElementById('move').addEventListener('click', function() {
-//     document.getElementById('text').classList.toggle('hidden')
-// })
