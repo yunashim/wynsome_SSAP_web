6fb520b7dee9b8e7eee4007e4de265a4fd6cff65
jo-yuni <84690460+jo-yuni@users.noreply.github.com>
2021-06-22 11:03:26 +0900

    받아라

--- docs/SSAP/SSAP.js
+++ docs/SSAP/SSAP.js
@@ -1,11 +1,13 @@
 document.getElementById('toggle-menu').addEventListener('click', function () {
     document.querySelector('.newmenu').classList.toggle('hidden');
 })
-function move() {
+
+document.getElementById('firstcircle').addEventListener('click', function () {
     document.querySelector(".firstcircle").classList.toggle("go")
     document.querySelector(".firsttext").classList.toggle("see")
-}
-function move1() {
+})
+
+document.getElementById('secondcircle').addEventListener('click', function () {
     document.querySelector(".secondcircle").classList.toggle("go")
     document.querySelector(".secondtext").classList.toggle("see")
-}
+})
