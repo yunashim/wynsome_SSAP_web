939817926b4bcb34125313945e63689695c1730f
aethlis <84690253+aethlis@users.noreply.github.com>
2021-06-22 23:09:16 +0900

    changed some code for the videos

--- docs/SSAP_teaminfo/end-page.js
+++ docs/SSAP_teaminfo/end-page.js
@@ -9,3 +9,13 @@ function move1() {
     document.querySelector(".secondcircle").classList.toggle("go")
     document.querySelector(".secondtext").classList.toggle("see")
 }
+
+
+var perphoto = document.getElementsByClassName("personalPhoto")
+var perInfo = document.getElementsByClassName("")
+for (var i in perphoto) {
+    var photo = perphoto[i]
+    photo.addEventListener('click', function show() {
+        
+    })
+}
