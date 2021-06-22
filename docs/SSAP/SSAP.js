e4a5fd9b23de64595a38cc466957a4f893d1e5bc
jo-yuni <84690460+jo-yuni@users.noreply.github.com>
2021-06-22 11:16:36 +0900

    SSAP 사진 클릭 안되는

--- docs/SSAP/SSAP.js
+++ docs/SSAP/SSAP.js
@@ -1,7 +1,3 @@
-document.getElementById('toggle-menu').addEventListener('click', function () {
-    document.querySelector('.newmenu').classList.toggle('hidden');
-})
-
 document.getElementById('firstcircle').addEventListener('click', function () {
     document.querySelector(".firstcircle").classList.toggle("go")
     document.querySelector(".firsttext").classList.toggle("see")
@@ -10,4 +6,8 @@ document.getElementById('firstcircle').addEventListener('click', function () {
 document.getElementById('secondcircle').addEventListener('click', function () {
     document.querySelector(".secondcircle").classList.toggle("go")
     document.querySelector(".secondtext").classList.toggle("see")
+})
+
+document.getElementById('toggle-menu').addEventListener('click', function () {
+    document.querySelector('.newmenu').classList.toggle('show');
 })
