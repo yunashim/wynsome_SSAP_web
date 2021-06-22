6847b4736d47d6e6cccb166f5f0be0a440ac42aa
aethlis <84690253+aethlis@users.noreply.github.com>
2021-06-23 07:38:47 +0900

    changed some code of the header..

--- docs/SSAP_teaminfo/end-page.js
+++ docs/SSAP_teaminfo/end-page.js
@@ -1,11 +1,3 @@
 document.getElementById('toggle-menu').addEventListener('click', function () {
-    document.querySelector('.newmenu').classList.toggle('hidden');
+    document.querySelector('.newmenu').classList.toggle("show");
 })
-function move() {
-    document.querySelector(".firstcircle").classList.toggle("go")
-    document.querySelector(".firsttext").classList.toggle("see")
-}
-function move1() {
-    document.querySelector(".secondcircle").classList.toggle("go")
-    document.querySelector(".secondtext").classList.toggle("see")
-}
